b72088e77d26a222d0b20fcaa5ebd9ba7e2ff35e
ashinde2 <ashinde2@illinois.edu>
2021-05-11 23:46:29 -0400

    added remote control

--- MagicMirror/config/config.js
+++ MagicMirror/config/config.js
@@ -10,6 +10,7 @@
 
 var config = {
 	address: "localhost", 	// Address to listen on, can be:
+	// address: "0.0.0.0",
 	// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
 	// - another specific IPv4/6 to listen on a specific interface
 	// - "0.0.0.0", "::" to listen on any interface
@@ -18,6 +19,7 @@ var config = {
 	basePath: "/", 	// The URL path where MagicMirror is hosted. If you are using a Reverse proxy
 	// you must set the sub path here. basePath must end with a /
 	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], 	// Set [] to allow all IP addresses
+	// ipWhitelist: [],
 	// or add a specific IPv4 of 192.168.1.5 :
 	// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
 	// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
@@ -62,6 +64,7 @@ var config = {
 				sports: [
 					{
 						league: "NBA",
+						groups: ["West"]
 					},
 					{
 						league: "UEFA_CHAMPIONS",
@@ -135,6 +138,21 @@ var config = {
 				showPublishDate: true,
 				broadcastNewsFeeds: true,
 				broadcastNewsUpdates: true
+			}
+		},
+		{
+			module: 'MMM-Remote-Control',
+			// uncomment the following line to show the URL of the remote control on the mirror
+			position: 'bottom_left',
+			// you can hide this module afterwards from the remote control itself
+			config: {
+				customCommand: {},  // Optional, See "Using Custom Commands" below
+				showModuleApiMenu: true, // Optional, Enable the Module Controls menu
+				secureEndpoints: true, // Optional, See API/README.md
+				// uncomment any of the lines below if you're gonna use it
+				// customMenu: "custom_menu.json", // Optional, See "Custom Menu Items" below
+				// apiKey: "", // Optional, See API/README.md for details
+				// classes: {} // Optional, See "Custom Classes" below
 			}
 		},
 	]
